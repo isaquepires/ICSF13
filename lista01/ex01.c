038e9a15bea20e1595207bc6b0d40bc67143189d
Isaque <164631669+isaquepires@users.noreply.github.com>
2024-03-25 18:30:16 -0300

    Add files via upload

---
+++ lista01/ex01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main ()
+{
+	int numero, unidade, dezena, centena, resposta;
+
+    	scanf("%d", &numero);
+
+    	// isolando casas numéricas
+    	unidade = numero % 10;
+    	dezena = (numero % 100)/10;
+    	centena = numero / 100;
+
+    	resposta = unidade * 100 + dezena * 10 + centena;
+    	printf("%d\n", resposta);
+	
+	return 0;
+
+}
