a1c85ea33a8d1ab7ffe1884f18dd05339eb722a2
Isaque <164631669+isaquepires@users.noreply.github.com>
2024-03-25 18:09:38 -0300

    Add files via upload

---
+++ lista00/ex02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main ()
+{
+	int num, dias, horas, minutos, segundos;
+
+	scanf("%d", &num);
+
+	dias = num / 86400;
+	horas = (num % 86400) / 3600;
+	minutos = ((num % 86400) % 3600) / 60;
+	segundos = ((num % 86400) % 3600) % 60;
+	printf("%d\n%d\n%d\n%d\n", dias, horas, minutos, segundos);
+
+	return 0;
+}
