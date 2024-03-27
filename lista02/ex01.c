4559a30ec62ef12319f3309b020c8f7ad0cc32fe
Isaque Pires <isaquepires@protonmail.com>
2024-03-27 20:53:26 -0300

    Add files via upload

---
+++ lista02/ex01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+	int dia_nasc, mes_nasc, ano_nasc,
+	dia_atual, mes_atual, ano_atual, idade;
+
+	scanf("%d %d %d %d %d %d", &dia_nasc, &mes_nasc, &ano_nasc, &dia_atual, &mes_atual, &ano_atual);
+	
+	idade = ano_atual - ano_nasc;
+
+	if (mes_atual < mes_nasc || mes_atual == mes_nasc && dia_atual < dia_nasc)
+		idade = idade - 1;
+		
+	printf("%d\n", idade);
+}
