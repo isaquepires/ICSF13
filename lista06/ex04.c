fee3a4a1d1f9c7da2f9927dae227159cf6474886
Isaque Pires <isaquepires@protonmail.com>
2024-04-18 07:15:08 -0300

    Add files via upload

---
+++ lista06/ex04.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#define CENTRO 2*n+1
+
+int main()
+{
+    int n, cont_branco, cont_carac, meio, cima, branco, baixo;
+
+    char caractere;
+
+    scanf ("%c", &caractere);
+    scanf ("%d", &n);
+
+    branco = 0;
+
+    // CIMA
+    for (cima = 0; cima < n; cima++)
+    {
+        for (cont_branco = 0; cont_branco < n - branco; cont_branco++)
+            printf(" ");
+
+        for (cont_carac = 0; cont_carac < CENTRO - 2*cont_branco; cont_carac++)
+            printf("%c", caractere);
+
+        // incrementa aumentando espacos
+        branco++;
+        printf("\n");
+    }
+
+    // MEIO
+    for (meio = 0; meio < CENTRO; meio++)
+        printf("%c", caractere);
+
+    printf("\n");
+
+    // BAIXO
+    branco = n-1;
+
+    for (baixo = 0; baixo < n; baixo++)
+    {
+        for (cont_branco = 0; cont_branco < n - branco; cont_branco++)
+            printf(" ");
+
+        for (cont_carac = 0; cont_carac < CENTRO - 2*cont_branco; cont_carac++)
+            printf("%c", caractere);
+
+        // incrementa diminuindo espacos
+        branco--;
+        printf("\n");
+    }
+
+    return 0;
+}
