c7cd2c8f8ae0b720c76e3a4975ad39be16c1c56d
Isaque Pires <isaquepires@protonmail.com>
2024-04-17 21:55:14 -0300

    Add files via upload

--- lista05/ex06.c
+++ lista05/ex06.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 
-// 2 3 3 3 2 1
-
 int main()
 {
     int n, i, valor, sequencia, consecutivos, valor_anterior, maior_sequencia;
@@ -21,22 +19,17 @@ int main()
         scanf("%d", &valor);
 
         if (valor == valor_anterior)
-        {
             consecutivos++;
-        }
         else
             consecutivos = 1;
 
         if (consecutivos > maior_sequencia)
             maior_sequencia = consecutivos;
 
-        printf("consec: %d\n", maior_sequencia);
-
         valor_anterior = valor;
-
     }
 
-    printf("%d\n", maior_sequencia);
+    printf("maior sequencia: %d\n", maior_sequencia);
 
     return 0;
 }
