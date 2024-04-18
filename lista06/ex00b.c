8519046d01ad8456b6fc1a1daf28e3d0a8471308
Isaque Pires <isaquepires@protonmail.com>
2024-04-17 22:03:13 -0300

    Add files via upload

---
+++ lista06/ex00b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i, j, k;
+    for (i = 1; i <= 6; i++)
+        for (j = 1; j <= 6; j++)
+            for (k = 1; k <= 6; k++)
+                printf ("%d, %d e %d\n", i, j, k);
+
+    return 0;
+}
