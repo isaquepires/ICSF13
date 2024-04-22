2e8ecb991f711add2408f3f4f114e2dbbc8e0280
Isaque Pires <isaquepires@protonmail.com>
2024-04-22 18:15:58 -0300

    Add files via upload

---
+++ lista09/ex00.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void segundosParaHMS (int total_segundos, int *h, int *m, int *s)
+{
+    *h = total_segundos / 3600;
+    *m = (total_segundos % 3600) / 60;
+    *s = (total_segundos % 3600) % 60;
+
+}
+
+int main()
+{
+    int segundos, hor, min, seg;
+
+    segundos = 10000;
+
+    segundosParaHMS(segundos, &hor, &min, &seg);
+
+    printf("%d:%d:%d\n", hor, min, seg);
+
+
+    return 0;
+}
